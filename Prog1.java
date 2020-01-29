59114e960143fadf500c3153080007ac33ee4246
Numan Ahmed <39140801+mdnmnahmed@users.noreply.github.com>
2020-01-29 12:50:28 +0530

    Java Prog1

---
+++ Prog1.java
@@ -0,0 +1,8 @@
+class Prog1{
+    public static void main(String [] args){
+        System.out.println("Hello Numan");
+        System.out.println("Hello Ahmed");
+        int n1 = 10, n2=20;
+        System.out.println(n1 + n2);
+    }
+}
